a4c6641a1f4dd40f28108bb3f6d48758dafad674
Vladimir Trukhin <vvt@narod.ru>
2016-09-28 02:48:03 +0400

    Create narrow.module.js

---
+++ module3-solution/scripts/narrow.module.js
@@ -0,0 +1,9 @@
+(function () {
+'use strict';
+
+	angular.module('NarrowItDownApp', ['LoaderApp'])
+		.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+
+	//console.log("Initializing narrow.module..");
+
+})();
