b3de4efedf06f038064b81bc32820510a1423191
Vladimir Trukhin <vvt@narod.ru>
2016-09-28 02:41:37 +0400

    Create loader.component.js

---
+++ module3-solution/scripts/loader.component.js
@@ -0,0 +1,20 @@
+(function () {
+'use strict';
+
+	angular.module('LoaderApp')
+		.component('itemsLoaderIndicator', {
+			templateUrl: 'loader/itemsloaderindicator.template.html',
+			controller: loaderComponentController,
+			bindings: {
+				showLoader: '<'
+			}
+		})
+  ;
+
+	loaderComponentController.$inject = ['$scope'];
+	function loaderComponentController($scope) {
+		var $ctrl = this;
+		//console.log('Initializing loader.component..');
+	}
+
+})();
