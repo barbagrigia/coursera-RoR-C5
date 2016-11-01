1f0a1c10e25a5ad88e434fa3ecd2657987350dbe
Vladimir Trukhin <vvt@narod.ru>
2016-11-01 04:47:06 +0400

    Delete myinfo.component.js

--- module5-solution/src/public/myinfo/myinfo.component.js
+++
@@ -1,9 +0,0 @@
-(function () {
-    'use strict';
-    angular.module('public').component('menu-item', {
-        templateUrl: 'src/public/myinfo/menu-item.component.html'
-        , bindings: {
-            menuItem: '<'
-        , }
-    });
-})();
