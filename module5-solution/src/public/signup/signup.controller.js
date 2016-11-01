d86238e528f21676cbc3e7c7193e573abb0dcdfa
Vladimir Trukhin <vvt@narod.ru>
2016-11-01 04:08:24 +0400

    Update signup.controller.js

--- module5-solution/src/public/signup/signup.controller.js
+++ module5-solution/src/public/signup/signup.controller.js
@@ -1,45 +1,18 @@
 (function () {
-    "use strict";
-    angular.module('public').controller('SignupController', SignupController);
-    SignupController.$inject = ['serverMenuService'];
+"use strict";
 
-    function SignupController(serverMenuService) {
-        var ctrl = this;
-        ctrl.favDish = '';
-        ctrl.errorResponse = '';
-        ctrl.menuItem = '';
-        ctrl.firstname = '';
-        ctrl.lastname = '';
-        ctrl.email = '';
-        ctrl.phonenumber = '';
-        ctrl.submit = function () {
-            //console.log(ctrl.firstname);
-            //console.log(ctrl.lastname);
-            //console.log(ctrl.email);
-            //console.log(ctrl.phonenumber);
-            var promise = serverMenuService.getFavouriteMenuItem(ctrl.favDish);
-            promise.then(function (response) {
-                ctrl.menuItem = response;
-                //console.log(ctrl.menuItem);
-            }).then(function () {
-                if (ctrl.menuItem.status == 500 && ctrl.menuItem.statusText == "Internal Server Error") {
-                    ctrl.errorResponse = 'No such ' + ctrl.favDish + ' menu number exists';
-                }
-                else {
-                    serverMenuService.storeFavourireMenuItem(ctrl.menuItem);
-                    serverMenuService.storeUserData(ctrl.firstname, ctrl.lastname, ctrl.email, ctrl.phonenumber);
-                    serverMenuService.reg();
-                    ctrl.firstname = '';
-                    ctrl.lastname = '';
-                    ctrl.email = '';
-                    ctrl.phonenumber = '';
-                    ctrl.favDish = '';
-                    ctrl.errorResponse = 'Your information has been saved';
-                }
-            }).catch(function (error) {
-                console.log(error);
-            });
-            //console.log(ctrl.menuItem);
-        };
-    }
+angular.module('public')
+.controller('SignUpController', SignUpController);
+
+SignUpController.$inject = ['SignUpService'];
+function SignUpController(SignUpService) {
+  var SignUpCtrl = this;
+
+  SignUpCtrl.submit = function () {
+    SignUpService.saveUser(SignUpCtrl.user);
+    console.log(SignUpCtrl.user);
+    SignUpCtrl.completed = true;
+  };
+}
+
 })();
